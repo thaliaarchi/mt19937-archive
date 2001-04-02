9378b1728091433df25c38d79932aa56ebd8b09e
Makoto Matsumoto <matumoto@math.keio.ac.jp>
2001-04-02 10:48:59 +0000

    Relicense for release
    https://web.archive.org/web/20010824083746/http://www.math.keio.ac.jp/matumoto/mt19937.tar

--- mt19937-1.c
+++ mt19937-1.c
@@ -8,19 +8,10 @@
 /*   Coded by Takuji Nishimura, considering the suggestions by  */
 /* Topher Cooper and Marc Rieffel in July-Aug. 1997.            */
 
-/* This library is free software; you can redistribute it and/or   */
-/* modify it under the terms of the GNU Library General Public     */
-/* License as published by the Free Software Foundation; either    */
-/* version 2 of the License, or (at your option) any later         */
-/* version.                                                        */
-/* This library is distributed in the hope that it will be useful, */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of  */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.            */
-/* See the GNU Library General Public License for more details.    */
-/* You should have received a copy of the GNU Library General      */
-/* Public License along with this library; if not, write to the    */
-/* Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA   */ 
-/* 02111-1307  USA                                                 */
+/* This library is free software under the Artistic license:       */
+/* see the file COPYING distributed together with this code.       */
+/* For the verification of the code, its output sequence file      */
+/* mt19937-1.out is attached (2001/4/2)                           */
 
 /* Copyright (C) 1997, 1999 Makoto Matsumoto and Takuji Nishimura. */
 /* Any feedback is very welcome. For any question, comments,       */
